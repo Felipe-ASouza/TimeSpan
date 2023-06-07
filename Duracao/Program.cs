06e46fa4c540f9cf31a7e092facd44d646169cbb
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-07 00:07:56 -0300

    Controle de duracao/tempo com TimeSpan Csharp

---
+++ Duracao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Duracao
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            TimeSpan t1 = TimeSpan.FromDays(1.5);
+            TimeSpan t2 = TimeSpan.FromHours(1.5);
+            TimeSpan t3 = TimeSpan.FromHours(1.5);
+            TimeSpan t4 = TimeSpan.FromSeconds(1.5);
+            
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+        }
+    }
+}
